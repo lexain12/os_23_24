36d779100287914a14963f489e7dc4808645312b
Viktor <lexain12@gmail.com>
2023-11-28 23:09:06 +0300

    working version, needs to add readme and script

--- task_5/tiazhelkov.ve/sig_rcv.c
+++ task_5/tiazhelkov.ve/sig_rcv.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <assert.h>
 
-void sigusr1_handler(int sig, siginfo_t* info, void* context) {
-}
+void rcv_file(FILE* output);
 
-void sigusr2_handler(int sig, siginfo_t* info, void* context) {
-}
-
-void sigint_handler(int sig, siginfo_t* info, void* context) {
-}
+int END = 0;
+char VAL[4]   = {};
+int WRITE     = 0;
+pid_t SND_PID = 0;
+void configure_signals();
+void sigusr1_handler(int sig, siginfo_t* info, void* context);
+void sigusr2_handler(int sig, siginfo_t* info, void* context);
+void sigint_handler(int sig, siginfo_t* info, void* context);
 
 int main(int argc, char* argv[]) {
 
@@ -18,7 +21,60 @@ int main(int argc, char* argv[]) {
         printf ("Usage: ./sig_rcv outputFileName\n");
         return 0;
     }
+    printf ("my pid %d\n", getpid());
+    configure_signals();
 
+    FILE* outputFile = fopen(argv[1], "wb");
+    assert (outputFile != NULL);
     
+    rcv_file(outputFile);
 
+    fclose(outputFile);
+}
+
+void configure_signals() {
+    union sigval val;
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = sigusr1_handler;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    sa.sa_sigaction = sigusr2_handler;
+    sigaction(SIGUSR2, &sa, NULL);
+
+    sa.sa_sigaction = sigint_handler;
+    sigaction(SIGINT, &sa, NULL);
+}
+
+void rcv_file(FILE* output) {
+    while (1) {
+        pause();
+        if (WRITE) {
+            fwrite(VAL, (int) VAL[3], sizeof(char), output);
+            WRITE = 0;
+        }
+
+        if (END)  {
+            kill(SND_PID, SIGINT);
+            return;
+        }
+
+        kill (SND_PID, SIGINT);
+    }
+}
+
+void sigusr1_handler(int sig, siginfo_t* info, void* context) {
+    *((int*) VAL) = info->si_value.sival_int;
+    WRITE = 1;
+    return;
+}
+
+void sigusr2_handler(int sig, siginfo_t* info, void* context) {
+    END = 1;
+    return;
+}
+
+void sigint_handler(int sig, siginfo_t* info, void* context) {
+    SND_PID = info->si_value.sival_int;
+    return;
 }
--- task_5/tiazhelkov.ve/sig_snd.c
+++ task_5/tiazhelkov.ve/sig_snd.c
@@ -1,6 +1,8 @@
+#include <bits/types/siginfo_t.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <assert.h>
@@ -9,12 +11,18 @@ const size_t MAX_BUFF_SIZE = 4096;
 
 void snd_pid (pid_t rcv_pid);
 void snd_file (FILE* input_file, pid_t rcv_pid);
+void sigint_handler(int, siginfo_t*, void*);
+void configure_signals();
+
+void sigint_handler(int sig, siginfo_t* info, void* context) {};
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         printf ("Usage: ./sig_snd filename rcv_pid\n");
         return 0;
     }
+    configure_signals();
+
     pid_t rcv_pid = atoi(argv[2]);
 
     FILE* input_file = fopen(argv[1], "rb");
@@ -33,15 +41,25 @@ void snd_file (FILE* input_file, pid_t rcv_pid) {
     char buffer[MAX_BUFF_SIZE];
     int num_readed = 0;
 
+    // Здесь я передаю по три байта, где в последнем байте храниться длина данного сообщения 
     while (num_readed = fread(buffer, sizeof(char), MAX_BUFF_SIZE, input_file)) {
-        for (int i = 0; num_readed - i > 4; i+=4) {
-            val.sival_int = *(int*)(buffer + i);
+        int i = 0;
+        char package[4] = {};
+        for (; num_readed - i > 3; i+=3) {
+            memcpy(package, buffer + i, 3);
+            package[3] = 0x03;
+            val.sival_int = *(int*) package;
+            sigqueue (rcv_pid, SIGUSR1, val);
+            pause();
+        }
+        if (i < num_readed) {
+            memcpy(package, buffer + i, num_readed - i);
+            package[3] = num_readed - i;
+            val.sival_int = *(int*) package;
             sigqueue (rcv_pid, SIGUSR1, val);
             pause();
         }
 
-        for (; i < num_readed; i++) {
-        }
     }
 
     kill(rcv_pid, SIGUSR2);
@@ -53,4 +71,11 @@ void snd_pid (pid_t rcv_pid) {
     union sigval val;
     val.sival_int = getpid();
     sigqueue(rcv_pid, SIGINT, val);
+}
+
+void configure_signals() {
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = sigint_handler;
+    sigaction(SIGINT, &sa, NULL); 
 }
